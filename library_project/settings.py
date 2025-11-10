adc35425bcc58064ca360f0cac0520d3c940e006
moro23 <billhab66@gmail.com>
2025-11-10 12:07:45 +0000

    Add 'rest_framework' to INSTALLED_APPS in settings.py

--- library_project/settings.py
+++ library_project/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #3rd party
+    'rest_framework',
+
+    # Local
     'books.apps.BooksConfig',
+    'apis.apps.ApisConfig',
 ]
 
 MIDDLEWARE = [
